7012cd1669ba0ef52e8eea44024c1e7110a92aa7
Mauricio Nehuen Velazquez <mauri_i9070@hotmail.com>
2024-11-21 03:51:00 -0300

    cambio en las configuraciones de la bd 2

--- config/db.js
+++ config/db.js
@@ -6,34 +6,18 @@ export const db = async () => {
 		const db = await mongoose.connect(process.env.MONGO_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
-			serverSelectionTimeoutMS: 5000,
-			socketTimeoutMS: 45000,
-			replicaSet: 'rs0', // Nombre del replica set
-			readPreference: 'primaryPreferred', // Preferencia de lectura, primario preferido pero permite secundario
-			writeConcern: { w: 'majority', j: true }, // Garantiza que la escritura se propague en el replica set
+			replicaSet: 'rs0', // Asegúrate de especificar el nombre del replica set
+			readPreference: 'primaryPreferred', // Prioriza leer desde el primario, pero permitirá leer del secundario si el primario no está disponible
+			socketTimeoutMS: 30000, // Tiempo de espera de socket
+			connectTimeoutMS: 10000, // Tiempo de espera para establecer la conexión
+			serverSelectionTimeoutMS: 5000, // Timeout para seleccionar el servidor
+			heartbeatFrequencyMS: 1000, // Frecuencia de sondeo del replica set
+			useUnifiedTopology: true, // Usar el nuevo motor de topología para la reconexión automática
 		})
-
 		const url = `${db.connection.host}:${db.connection.port}`
 		console.log(colors.cyan(`MongoDB se conectó correctamente: ${url}`))
-
-		// Manejo de eventos de conexión
-		mongoose.connection.on('connected', () => {
-			console.log(colors.green('Conexión a MongoDB establecida.'))
-		})
-
-		mongoose.connection.on('disconnected', () => {
-			console.log(colors.red('Conexión a MongoDB perdida.'))
-		})
-
-		mongoose.connection.on('error', err => {
-			console.error(colors.red(`Error de conexión a MongoDB: ${err.message}`))
-		})
-
-		mongoose.connection.on('reconnected', () => {
-			console.log(colors.green('Conexión a MongoDB reconectada.'))
-		})
 	} catch (error) {
-		console.error(colors.red(`Error al conectar con MongoDB: ${error.message}`))
-		process.exit(1) // Detener la aplicación si no se puede conectar
+		console.log(`Error: ${error.message}`)
+		process.exit(1)
 	}
 }
