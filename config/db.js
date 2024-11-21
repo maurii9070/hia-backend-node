33d8ecabd64d006f92d891f0057227a3b6dce2db
RocioAV <44351449@fi.unju.edu.ar>
2024-11-21 01:23:30 -0300

    Cambios en config db

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
-import mongoose from 'mongoose'
-import colors from 'colors'
+import mongoose from 'mongoose';
+import colors from 'colors';
 
 export const db = async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URI)
-        const url = `${db.connection.host}:${db.connection.port}`
-        console.log( colors.cyan(`MongoDB se conectó correctamente: ${url}`))
+        const db = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            replicaSet: 'rs0',  // Asegúrate de especificar el nombre del replica set
+            readPreference: 'primaryPreferred',  // Prioriza leer desde el primario, pero permitirá leer del secundario si el primario no está disponible
+        });
+        const url = `${db.connection.host}:${db.connection.port}`;
+        console.log(colors.cyan(`MongoDB se conectó correctamente: ${url}`));
     } catch (error) {
-        console.log(`Error: ${error.message}`)
-        process.exit(1)
+        console.log(`Error: ${error.message}`);
+        process.exit(1);
     }
-}
+};
