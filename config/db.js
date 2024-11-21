fa4534723396ad605940202e07e633b31a4c83ca
Mauricio Nehuen Velazquez <mauri_i9070@hotmail.com>
2024-11-21 02:26:08 -0300

    cambios en la conexion de mongo

--- config/db.js
+++ config/db.js
@@ -1,18 +1,32 @@
-import mongoose from 'mongoose';
-import colors from 'colors';
+import mongoose from 'mongoose'
+import colors from 'colors'
 
 export const db = async () => {
-    try {
-        const db = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            replicaSet: 'rs0',  // Asegúrate de especificar el nombre del replica set
-            readPreference: 'primaryPreferred',  // Prioriza leer desde el primario, pero permitirá leer del secundario si el primario no está disponible
-        });
-        const url = `${db.connection.host}:${db.connection.port}`;
-        console.log(colors.cyan(`MongoDB se conectó correctamente: ${url}`));
-    } catch (error) {
-        console.log(`Error: ${error.message}`);
-        process.exit(1);
-    }
-};
+	try {
+		const db = await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true, // Opcional en versiones recientes
+			useUnifiedTopology: true, // Opcional en versiones recientes
+			serverSelectionTimeoutMS: 5000, // Tiempo de espera para encontrar un nodo disponible
+			socketTimeoutMS: 45000, // Tiempo máximo permitido para operaciones largas
+		})
+
+		const url = `${db.connection.host}:${db.connection.port}`
+		console.log(colors.cyan(`MongoDB se conectó correctamente: ${url}`))
+
+		// Eventos para diagnosticar problemas
+		mongoose.connection.on('connected', () => {
+			console.log(colors.green('Conexión a MongoDB establecida.'))
+		})
+
+		mongoose.connection.on('disconnected', () => {
+			console.log(colors.red('Conexión a MongoDB perdida.'))
+		})
+
+		mongoose.connection.on('error', err => {
+			console.error(colors.red(`Error de conexión a MongoDB: ${err.message}`))
+		})
+	} catch (error) {
+		console.error(colors.red(`Error al conectar con MongoDB: ${error.message}`))
+		process.exit(1) // Detener la aplicación si no se puede conectar
+	}
+}
